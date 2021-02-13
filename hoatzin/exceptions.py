5b53ae77f0326c60634de4caccbbe714c45c6c0e
bwoodsend <bwoodsend@gmail.com>
2021-02-13 01:43:59 +0000

    Allow HashTable.unique to be released as a writeable array.

--- hoatzin/exceptions.py
+++ hoatzin/exceptions.py
@@ -18,3 +18,12 @@ class HashTableFullError(HoatzinException):
     """Raised on attempting to add a new item to a table when the table is full.
     """
     pass
+
+
+class HashTableDestroyed(HoatzinException):
+    """Raised on adding or getting from a :class:`hoatzin.HashTable` which has
+    called :meth:`hoatzin.HashTable.destroy`."""
+
+    def __str__(self):
+        return "This table has been destroyed by HashTable.destroy() and can " \
+               "no longer be used."
