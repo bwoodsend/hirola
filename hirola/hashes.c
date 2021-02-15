efddd8ce67c24733bd1fdd7bef01b187cee796d2
bwoodsend <bwoodsend@gmail.com>
2021-02-15 21:23:12 +0000

    Consistently use int32 for hashes.
    Avoids headache-inducing deviation between 64 and 32 bit platforms.

--- hirola/hashes.c
+++ hirola/hashes.c
@@ -1,6 +1,7 @@
 // -*- coding: utf-8 -*-
 
 #include <string.h>
+#include <stdint.h>
 #include "hashes.h"
 
 
@@ -11,12 +12,12 @@
 
   Hashes must:
 
-  - Accept arbitrary blob of data and return a ptrdiff_t integer.
+  - Accept arbitrary blob of data and return a int32_t integer.
   - Be deterministic. hash(x) == hash(x)
   - Be as chaotic as possible.hash(b"hello") should be completely different to
     hash(b"hellO").
   - Break up consecutive or small range values. hash(3) and hash(4) should be
-    very different and large, utilising the full range of a ptrdiff_t.
+    very different and large, utilising the full range of a int32_t.
   - Mustn't be more likely to yield some answers more than others. This means
     that if you use multiplication in a hash, it must be with a prime number
     and large enough induce overflow. Otherwise the output is limited to only
@@ -31,27 +32,27 @@
  */
 
 
-ptrdiff_t hash(void * key, const size_t key_size) {
+int32_t hash(void * key, const size_t key_size) {
   /* Generic hash - assumes **key_size** is a multiple of sizeof(int32_t). */
 
   int32_t * key_ = (int32_t *) key;
-  ptrdiff_t out = 0;
+  int32_t out = 0;
   for (size_t i = 0; i < key_size / sizeof(int32_t); i++)
       out ^= key_[i] * 0x0B070503;
   return out;
 }
 
 
-ptrdiff_t small_hash(void * key, const size_t key_size) {
+int32_t small_hash(void * key, const size_t key_size) {
   /* Hash for key_size <= sizeof(int32_t). */
 
-  ptrdiff_t out = 0;
+  int32_t out = 0;
   memcpy(&out, key, key_size);
   return out * 0x0B070503;
 }
 
 
-ptrdiff_t hybrid_hash(void * key, const size_t key_size) {
+int32_t hybrid_hash(void * key, const size_t key_size) {
   /* A combination of hash() and small_hash() for when **key_size** is not a
      multiple of sizeof(int32_t). */
 
