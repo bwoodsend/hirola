f3f03658b8c714b89f0dbf1a5e122992810a65bc
bwoodsend <bwoodsend@gmail.com>
2021-09-01 09:20:46 +0100

    Reformat the PyInstaller hook.

--- hirola/hook-hirola.py
+++ hirola/hook-hirola.py
@@ -5,5 +5,4 @@ Hook for PyInstaller.
 
 from hirola._hash_table import slug
 
-datas = [(str(slug.path), "hirola"),
-         (str(slug.types_map.json_path), "hirola")]
+datas = [(str(slug.path), "hirola"), (str(slug.types_map.json_path), "hirola")]
