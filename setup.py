fd3f4847085e94ac9d89bf52a5e10a16b685a1af
bwoodsend <bwoodsend@gmail.com>
2021-10-05 23:27:31 +0100

    Test and declare support for Python 3.10.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="NumPy vectorized hash table for fast set and dict operations.",
     install_requires=copy_requirements(),
